ba958f7a5fd88784775975b8bac5e67818a146f0
sarakjaer <sara.petersen@hotmail.com>
2017-02-15 15:49:18 +0100

    Add files via upload

---
+++ Cartoon, miniex02/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+createCanvas(displayWidth,displayHeight);
+}
+
+function draw() {
+  background(255, 255, 0);
+  fill(240, 255, 255); 
+  ellipse(56, 46, 55, 55);
+  triangle(80, 145, 60, 80, 200, 145);
+}
